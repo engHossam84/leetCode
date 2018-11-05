adad71b7c3057ca9dbb57dd8fce742a780fe1658
engHossam84 <eng.hossam84@gmail.com>
2018-11-05 09:25:10 +0200

    Update LongestPalindromicSubstring.cpp

--- Medium/LongestPalindromicSubstring.cpp
+++ Medium/LongestPalindromicSubstring.cpp
@@ -192,6 +192,113 @@ public:
 		return result;
 
 	}
+	
+	//run time 1644 ms
+	string longestPalindrome4(string s) {
+        unordered_map<char,vector<int>> charList;
+        int forwardIdx;
+        int backwardIdx;
+        string output="";
+        bool isMatched = false;
+        if(s.length() < 2)
+            return s;
+        else
+        {
+            for(int i = 0 ; i < s.length() ; i ++)
+            {
+                //cout << "insert " << s.at(i) << endl;
+                charList[s.at(i)].push_back(i);
+            }
+            for(int i = 0 ; i < s.length() ; i ++)
+            {
+                auto got = charList.find(s.at(i));
+                vector<int> matches = got->second ;
+                int matchSize = matches.size() - 1;
+               /* if(matchSize == 0)
+                {
+                    break;
+                }*/
+                //cout << "matchSize of " << s.at(i) << " = " << matchSize << endl;
+                for(int j = matchSize  ; j >= 0 ; j --)
+                {
+                    if(matches[j] > i )
+                    {
+                        //cout << "i = " << i << " , matches[j] = " << matches[j] <<endl;
+                        if((matches[j]  - i + 1) % 2 != 0 )
+                        {
+                            int mid = ((matches[j] - i) / 2) + i;
+                            forwardIdx = mid +1;
+                            backwardIdx = mid - 1;
+                            while(forwardIdx  <= matches[j] && backwardIdx >= i)
+                            {
+                                //cout << "forwardIdx = " << forwardIdx << ", backwardIdx = " << backwardIdx <<endl;
+                               // cout << "s.at(forwardIdx) = " << s.at(forwardIdx) <<", s.at(backwardIdx) = " << s.at(backwardIdx) <<endl;
+                                if(s.at(backwardIdx) == s.at(forwardIdx) )
+                                {
+                                    isMatched = true;
+                                    forwardIdx ++;
+                                    backwardIdx --;
+                                    
+                                }
+                                else
+                                {
+                                    isMatched = false;
+                                    break;
+                                }
+                            }
+                            if(isMatched && output.length() <= (matches[j] - i + 1))
+                            {
+                                output = s.substr(i,matches[j] - i + 1 );
+                                isMatched = false;
+                                break;
+                            }
+                            
+                        }
+                        else
+                        {
+                            int mid1 = ((matches[j] - i) / 2) + i;
+                            int mid2 = mid1 + 1;
+                            forwardIdx = mid2;
+                            backwardIdx = mid1;
+                            //cout << "forwardIdx2 = " << forwardIdx << ", backwardIdx2 = " << backwardIdx << endl;
+                           //cout << "s.at(forwardIdx2) = " << s.at(forwardIdx) <<", s.at(backwardIdx2) = " << s.at(backwardIdx) <<endl;
+                             while(forwardIdx  <= matches[j] && backwardIdx >= i)
+                            {
+                                if(s.at(backwardIdx) == s.at(forwardIdx) )
+                                {
+                                    isMatched = true;
+                                    forwardIdx ++;
+                                    backwardIdx --;
+                                    
+                                }
+                                else
+                                {
+                                    isMatched = false;
+                                    break;
+                                }
+                            }
+                            if(isMatched && output.length() <= (matches[j] - i + 1))
+                            {
+                                output = s.substr(i,matches[j] - i + 1 );
+                                isMatched = false;
+                                break;
+                            }
+                        }
+                    }
+                    else
+                        break;
+                }
+                 
+            }
+            if(output.length() > 0)
+                return output;
+            else
+            {
+                output = s.at(s.length() -1);
+                return output;
+            }
+        }
+    }
 };
 
 void main()
