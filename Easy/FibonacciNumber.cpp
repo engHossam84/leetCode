9c891f5b1627592175a4c77ccaefa3c62ac3ce4a
engHossam84 <eng.hossam84@gmail.com>
2019-04-08 09:55:39 +0200

    Create FibonacciNumber.cpp

---
+++ Easy/FibonacciNumber.cpp
@@ -0,0 +1,59 @@
+/*
+The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, such that each number is the sum of the two preceding ones, starting from 0 and 1. That is,
+
+F(0) = 0,   F(1) = 1
+F(N) = F(N - 1) + F(N - 2), for N > 1.
+Given N, calculate F(N).
+
+
+
+Example 1:
+
+Input: 2
+Output: 1
+Explanation: F(2) = F(1) + F(0) = 1 + 0 = 1.
+Example 2:
+
+Input: 3
+Output: 2
+Explanation: F(3) = F(2) + F(1) = 1 + 1 = 2.
+Example 3:
+
+Input: 4
+Output: 3
+Explanation: F(4) = F(3) + F(2) = 2 + 1 = 3.
+*/
+
+class Solution {
+public:
+	int fib(int N) {
+		int x = 0, y = 1, z = 0;
+		if (N < 2)
+		{
+			return N;
+		}
+		for (int i = 1; i < N; i++) {
+			z = x + y;
+			x = y;
+			y = z;
+		}
+		return z;
+
+	}
+
+	/*  int fibHelper(int N, vector<int> &mem)
+	{
+
+	if(N < 2)
+	{
+	return N;
+	}
+
+	if(mem[N] != -1)
+	return mem[N];
+
+	mem[N] = fibHelper(N-1,mem)+ fibHelper(N-2,mem);
+	return mem[N];
+
+	}*/
+};
